9da1b7e2fbe3e3ec13b3c7487e6a737b095aea73
Caio <caio.gsg12@gmail.com>
2024-09-28 18:05:11 -0300

    Envio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone iphone = new Iphone("12345678", "Iphone 16", "000000000000", 256);
+
+Console.WriteLine("Aplicativo Iphone");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
+
+Smartphone nokia = new Nokia("12345678", "Iphone 16", "000000000000", 256);
+
+Console.WriteLine("Aplicativo Nokia");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
