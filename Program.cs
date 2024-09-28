59ea2c5c73f69261c29cc8b1006a475a57960e5c
Caio <109152281+GCaioS@users.noreply.github.com>
2024-09-28 18:03:58 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+Smartphone iphone = new Iphone("12345678", "Iphone 16", "000000000000", 256);
+
+Console.WriteLine("Aplicativo Iphone");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
+
+Smartphone nokia = new Nokia("12345678", "Iphone 16", "000000000000", 256);
+
+Console.WriteLine("Aplicativo Nokia");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
